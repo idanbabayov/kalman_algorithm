4dba6a0d03e00c52314030cd6a3aa3c35f08d154
idanbabayov <idanbabayov@gmail.com>
2025-01-24 13:07:52 +0200

    one sensor final kalman filter

--- kalman_algorithm/ekf_lidar_radar.py
+++ kalman_algorithm/ekf_lidar_radar.py
@@ -8,33 +8,20 @@ import numpy as np
 
 
 
-"""
-#Fill in Those functions:
-
-
-    
-
-    
-#
-
-    
-"""
 def computeCovMatrix(deltaT, sigma_aX, sigma_aY):
         # Covariance matrix (Sigma)
 # [
-#     [sigma_px^2, sigma_px_py, sigma_px_vx, sigma_px_vy, sigma_px_yaw, sigma_px_yawRate],  # Covariance between position x and other variables
-#     [sigma_py_px, sigma_py^2, sigma_py_vx, sigma_py_vy, sigma_py_yaw, sigma_py_yawRate],  # Covariance between position y and other variables
-#     [sigma_vx_px, sigma_vx_py, sigma_vx^2, sigma_vx_vy, sigma_vx_yaw, sigma_vx_yawRate],  # Covariance between velocity x and other variables
-#     [sigma_vy_px, sigma_vy_py, sigma_vy_vx, sigma_vy^2, sigma_vy_yaw, sigma_vy_yawRate],   # Covariance between velocity y and other variables
-#     [sigma_yaw_px,sigma_yaw_py, sigma_yaw_vx, sigma_yaw_vy, sigma_yaw^2, sigma_yaw_yawRate ], # Covariance between yaw and other variables
-#     [sigma_yaw_px,sigma_yaw_py, sigma_yaw_vx, sigma_yaw_vy, sigma_yaw_yawRate, sigma_yaw_yawRate^2 ]  # Covariance between yawRate and other variables
+#     [sigma_px^2, sigma_px_py, sigma_px_vx, sigma_px_vy],  # Covariance between position x and other variables
+#     [sigma_py_px, sigma_py^2, sigma_py_vx, sigma_py_vy],  # Covariance between position y and other variables
+#     [sigma_vx_px, sigma_vx_py, sigma_vx^2, sigma_vx_vy,  # Covariance between velocity x and other variables
+#     [sigma_vy_px, sigma_vy_py, sigma_vy_vx, sigma_vy^2],   # Covariance between velocity y and other variables
 # ]
  
 # Example of a possible P matrix: Initial state covariance matrix P (6x6) with covariance between position and velocity
 # It is an initial guess of the uncertainty of the inital guess of the state.
 
 
-    cov = np.eye(6)  # Start with an identity matrix (or any other initial guess)
+    cov = np.eye(4)  # Start with an identity matrix (or any other initial guess)
     cov *= 1000      # Scale it to represent high initial uncertainty
     return cov
 
@@ -51,6 +38,7 @@ def computeRmse(trueVector, EstimateVector):
     squared_diff = (trueVector - EstimateVector) ** 2
 
     # Compute the mean squared error for each state component (across all measurements)
+    # Compute the mean across axis 0 (columns)
     mse = np.mean(squared_diff, axis=0)
 
     # Compute the RMSE by taking the square root of the mean squared error
@@ -66,9 +54,9 @@ def computeRadarJacobian(Xvector): # Jacobian matrix h with respect to state var
 
 # Jacobian matrix h with respect to state variables [Px, Py, Vx, Vy , yaw, yawRate]
 # Assuming h1(Px, Py, Vx, Vy) = rho =sqrt(Px^2+Py^2) , h2(Px, Py, Vx, Vy) = phi = tg^-1(Py/Px), and h3(Px, Py, Vx, Vy) = rho_dot =(Px*Vx+Py*Vy)/sqrt(Px^2+Py^2)
-# h = [[ dh1/dPx  dh1/dPy  dh1/dVx  dh1/dVy dh1/dyaw dh1/dyawRate],
-#      [ dh2/dPx  dh2/dPy  dh2/dVx  dh2/dVy dh2/dyaw dh2/dyawRate ],
-#      [ dh3/dPx  dh3/dPy  dh3/dVx  dh3/dVy dh3/dyaw dh3/dyawRate ]]
+# h = [[ dh1/dPx  dh1/dPy  dh1/dVx  dh1/dVy ],
+#      [ dh2/dPx  dh2/dPy  dh2/dVx  dh2/dVy  ],
+#      [ dh3/dPx  dh3/dPy  dh3/dVx  dh3/dVy ]]
 #when solving we get:
 # h = [[ d(sqrt(Px^2+Py^2))/dPx  d(sqrt(Px^2+Py^2))/dPy  d(sqrt(Px^2+Py^2))/dVx  d(sqrt(Px^2+Py^2))/dVy d(sqrt(Px^2+Py^2))/dyaw d(sqrt(Px^2+Py^2))/dyawRate],
 #      [ d(tg^-1(Py/Px))/dPx  d(tg^-1(Py/Px))/dPy d(tg^-1(Py/Px))/dVx  d(tg^-1(Py/Px))/dVy d(tg^-1(Py/Px))/dyaw d(tg^-1(Py/Px))/dyawRate ],
@@ -78,8 +66,7 @@ def computeRadarJacobian(Xvector): # Jacobian matrix h with respect to state var
     Py = Xvector[1] 
     vx = Xvector[2] 
     vy = Xvector[3]
-    yaw = Xvector[4]
-    yawRate = Xvector[5]
+
     #for conviniecnce let us define :
     P_square =Px**2+Py**2 
     h_radar = [[ Px/(P_square)**0.5 , Py/(P_square)**0.5 , 0 , 0 , 0 ,0],
@@ -94,50 +81,36 @@ def computeFmatrix(deltaT):
 # State vector: [Px, Py, Vx, Vy, Yaw, Yaw Rate]
 # Px, Py: Positions in x and y
 # Vx, Vy: Velocities in x and y
-# Yaw: Robot's orientation (angle in radians)
-# Yaw Rate: Rate of change of the yaw (angular velocity)
 
 
-#Px, Py, Vx, Vy, Yaw, Yaw_rate = state vector x 
+#Px, Py, Vx, Vy = state vector x 
 #The kinematic model equations are as follows:
 
 #1)Px_new = Px + Vx * delta_t
 #2)Py_new = Py + Vy * delta_t
 #3)Vx_new = Vx
 #4)Vy_new = Vy
-#5)Yaw_new = Yaw + Yaw_rate * delta_t
-#6)Yaw_rate_new = Yaw_rate
-
 #Remark: deltaT is not constant
 
-    F = np.matrix([ [1,0,deltaT,0,0,0],
-                    [0,1,0,deltaT,0,0],
-                    [0,0,1,0,0,0],
-                    [0,0,0,1,0,0],
-                    [0,0,0,0,1,deltaT],
-                    [0,0,0,0,0,1] ])
+    F = np.matrix([ [1,0,deltaT,0],
+                    [0,1,0,deltaT],
+                    [0,0,1,0],
+                    [0,0,0,1]])
 
     return F   
     
 def main():
-    # we print a heading and make it bigger using HTML formatting
-    print("Hellow")
     my_cols = ["A", "B", "C", "D", "E","f","g","h","i","j","k"]
     data = pd.read_csv("./src/kalman_algorithm/kalman_algorithm/data_radar_and_lidar.txt", names=my_cols, delim_whitespace = True, header=None)
     print(data.head())
-    for i in range(10):
-        measur = data.iloc[i,:].values
-        #print(measur[0])
 
     #define matrices:   
-    deltaT = 0.1 #known for an initial guess!
-    F_matrix = computeFmatrix(deltaT)
-
+    deltaT = 0.1#known for an initial guess!
 
     P = computeCovMatrix(deltaT, 1.0, 1.0)
 
-    H_Lidar = np.matrix([[1,0,0,0,0,0],
-                        [0,1,0,0,0,0]])
+    H_Lidar = np.matrix([[1,0,0,0],
+                        [0,1,0,0]])
     R_lidar = np.array([[0.0225, 0.0],
                         [0.0, 0.0225]]) #known
     R_radar = np.array([[0.9, 0, 0],
@@ -147,26 +120,25 @@ def main():
     xEstimate = []
     xTrue = []  
     #fill in X_true and X_state. Put 0 for the velocities
-    X_state_current = np.array([2.2,1.2,0.0,0.0,0.2,0.03]) #initial state guess
-    X_true_current = np.array([0.0,0.0,0.0,0.0,0.0,0.0]) #initial empty
+    X_state_current = np.array([2.2,1.2,0.0,0.0]) #initial state guess
+    X_true_current = np.array([0.0,0.0,0.0,0.0]) #initial empty
 
     firstMeasurment = data.iloc[0,:].values
     timeStamp = firstMeasurment[3]
-    counter = 0
     
     for i in range(1,len(data)):
         currentMeas = data.iloc[i,:].values
 
-        # compute the current dela t
+        # compute the current delta t
         if(currentMeas[0]=='L'):
             for j in range(4,len(X_true_current)):
                 X_true_current[j-4] = currentMeas[j] #for each measurement we can take from the table the true value of the specified measurement.
-            
+
             deltaT = (currentMeas[3]- timeStamp)/1000000
             timeStamp = currentMeas[3]
-            
+           
             #perfrom predict
-
+            F_matrix = computeFmatrix(deltaT)
             X_state_current =  F_matrix * X_state_current.reshape(-1,1)
             P  = F_matrix * P * F_matrix.transpose()
 
@@ -179,13 +151,12 @@ def main():
 
             X_state_current = X_state_current + K*(z-y)  #SEE THAT IT IS THE SAME! x + K * (Z - H * x ))
 
-            P = (np.eye(6) - (K * H_Lidar)) * P # this is simplification of P - K * H * P 
+            P = (np.eye(4) - (K * H_Lidar)) * P # this is simplification of P - K * H * P 
                         
-            xEstimate.append(X_state_current)  # Ensure it's 6x1
+            xEstimate.append(X_state_current) 
             xTrue.append(X_true_current.reshape(-1,1))
-    print(np.shape(xTrue))  # Should print (499, 6)
-    print(np.shape(xEstimate))  # Should print (499, 6)
-            
+    print(np.shape(xTrue))  
+    print(np.shape(xEstimate)) 
     rmse = computeRmse(xEstimate, xTrue) 
     print(rmse)
             
